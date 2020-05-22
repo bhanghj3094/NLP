1635720827e52af57dd587caf122c4a2d1550ee8
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-05-22 23:53:44 +0900

    feat: add hw3 skeletion

---
+++ HW3/CS372_HW3_code_20170305.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import nltk
+from nltk.corpus import wordnet as wn
+
+# Main function for word processing algorithm. 
+def main():
+    """
+    """
+
+
+if __name__ == "__main__":
+    main()
