c5291dcaeb48ac040335d511ffd3ecdb61c9b73d
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-05-23 23:24:03 +0900

    feat: add heteronyms

--- HW3/CS372_HW3_code_20170305.py
+++ HW3/CS372_HW3_code_20170305.py
@@ -4,10 +4,32 @@
 import nltk
 from nltk.corpus import wordnet as wn
 
+# Globals
+heteronyms = dict()
+
+
+def add_heteronyms(heteronyms):
+    """
+    Add heteronyms from wordnet synsets.
+    Update dictionary of heteronyms. 
+    {
+        word1: [(Pos1-1, definition1-1), (Pos1-2, definition1-2), ..],
+        word2: [(Pos2-1, definition2-1), (Pos2-2, definition2-2), ..],
+        ..
+    } # 'Pos' can be equal or different. 
+    """
+    for lemma_name in list(wn.all_lemma_names()):
+        heteronyms[lemma_name] = [
+            (synset.pos(), synset.definition())
+            for synset in wn.synsets(lemma_name)
+        ]
+
+
 # Main function for word processing algorithm. 
 def main():
     """
     """
+    add_heteronyms(heteronyms)
 
 
 if __name__ == "__main__":
