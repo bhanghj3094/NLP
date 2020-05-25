378faf3d4bac72660ffaecdd5829eca68471885e
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-05-25 14:51:47 +0900

    feat(crawling): build candidates, create heteronym list

--- HW3/CS372_HW3_code_20170305.py
+++ HW3/CS372_HW3_code_20170305.py
@@ -4,37 +4,136 @@
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import brown, cmudict
+from urllib import request
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+from pprint import pprint
+import time
 
 # Globals
-heteronyms = dict()
 heteronym_keys = []
-tagged_sentences = brown.tagged_sents()
-pronounce = cmudict.dict()
+heteronyms = dict()
+# tagged_sentences = brown.tagged_sents()
+cdict = cmudict.dict()
 
 
-def get_heteronyms(heteronyms, heteronym_keys):
+def get_heteronyms():
     """
-    Add heteronyms from wordnet synsets.
-    Update dictionary of heteronyms. 
-    {
-        word1: [(Pos1-1, definition1-1), (Pos1-2, definition1-2), ..],
-        word2: [(Pos2-1, definition2-1), (Pos2-2, definition2-2), ..],
-        ..
-    } # 'Pos' can be equal or different. 
     """
-    for lemma_name in list(wn.all_lemma_names()):
-        heteronyms[lemma_name] = [
-            (synset.pos(), synset.definition())
-            for synset in wn.synsets(lemma_name)
+    # Candidate for heteronyms
+    heteronym_keys.extend([ word
+        for word, pronounciations in cdict.items()
+        # Has different pronounciations
+        if len(pronounciations) >= 2
+        # Has more than one meaning
+        if len(wn.synsets(word)) >= 2
+    ])
+
+    # Web crawling
+    file = open("result.txt", "a")
+    for idx, word in enumerate(heteronym_keys):
+        entry = get_heteronym_entry(word)
+        # Insert to dictionary
+        if entry:
+            file.write("".join([word, ": ", str(entry), "\n"]))
+            heteronyms[word] = entry
+        time.sleep(1) # HTTPError code 429: Too Many Requests
+    file.close()
+
+
+def get_heteronym_entry(word):
+    """
+    Return list of pronounciations, part-of-speeches, and meanings.
+    [
+        {
+            pronounciation: [
+                (part-of-speech1, meaning1-1),
+                (part-of-speech1, meaning1-2),
+                (part-of-speech2, meaning2-1),
+                ...
+            ], ...
+        },
+        ...
+    ]
+    """
+    # urllib
+    url = "https://www.lexico.com/en/definition/"
+    try:
+        page = request.urlopen(url + word)
+    except HTTPError as e:
+        if e.code == 404:
+            return []
+        raise Exception
+    html = page.read().decode('utf8')
+
+    # beautifulsoup
+    soup = BeautifulSoup(html, 'html.parser')
+    homographs = soup.find_all(class_="entryGroup")
+
+    # not a heteronym
+    if len(homographs) <= 1: return []
+
+    # html content elements
+    contents = soup.find(class_="entryWrapper").contents
+    locations = [
+        contents.index(homograph)
+        for homograph in homographs
+    ]
+
+    # find pronounciation, part-of-speech, and meaning.
+    result = dict()
+
+    # exclude ones with all same pronounciation
+    try:
+        pronounciations = [
+            homograph.find_all(class_="phoneticspelling")[0].get_text()
+            for homograph in homographs
         ]
-    heteronym_keys.extend(heteronyms.keys())
+        # not a heteronym, but homograph
+        if len(set(pronounciations)) == 1: return []
+
+        for idx, homograph in enumerate(homographs):
+            # grambs: for each pronounciation, for each part-of-speech
+            start_location = locations[idx]
+            end_location = locations[idx+1] if idx < len(homographs) - 1 else len(contents)
+            grambs = [
+                content
+                for content in contents[start_location:end_location]
+                if 'class="gramb"' in str(content)
+            ]
+            
+            # add each part-of-speech with meaning.
+            meaning_list = []
+            for gramb in grambs:
+                pos = gramb.h3.find(class_="pos").get_text()
+                
+                # search only direct children
+                meaning_list.extend([ (
+                        pos, 
+                        meaning.find(class_="trg").p.find(class_="ind").get_text()
+                    )
+                    for meaning in gramb.ul.find_all("li", recursive=False)
+                ])
+
+            # insert to existing pronounciation, else create new entry.
+            try:
+                existing_list = result[pronounciations[idx]]
+                result[pronounciations[idx]] = existing_list + meaning_list
+            except KeyError:
+                result[pronounciations[idx]] = meaning_list
+    except AttributeError:
+        return []
+
+    # close and return entry
+    page.close()
+    return list(result.items())
 
 
 # Main function for word processing algorithm. 
 def main():
     """
     """
-    get_heteronyms(heteronyms, heteronym_keys)
+    get_heteronyms()
 
 
 if __name__ == "__main__":
