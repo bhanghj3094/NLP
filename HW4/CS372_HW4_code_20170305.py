aa509f5145605f028ff9b37a39952ba814456cb0
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-06-06 23:24:37 +0900

    feat(train_chunker): add regex parser

--- HW4/CS372_HW4_code_20170305.py
+++ HW4/CS372_HW4_code_20170305.py
@@ -31,12 +31,34 @@ def get_tagged_sentences():
     return sentences
 
 
+def train_chunker(sentences):
+    """
+    """
+    syntax = r"""
+        NP: {<DT|PP\$>?<JJ.*>*<NN.*>+ (<\(><NN.*><\)>)*}  # chunk determiner, adjectives, and noun
+            # }{                          # chinking
+    """
+    chunk_parser = nltk.RegexpParser(syntax)
+    for sentence in sentences:
+        text, triple = sentence
+        tokens = nltk.word_tokenize(text)
+        tagged = nltk.pos_tag(tokens)
+        chunked = chunk_parser.parse(tagged)
+        print(chunked)
+
+
 def main():
     # divide sentences into train, and test sets.
     sentences = get_tagged_sentences()
     random.shuffle(sentences)
     train = sentences[:80]
     test = sentences[80:]
+
+    # train chunker
+    train_chunker(test)
+
+    # detect_entities()
+    # extract_relations()
 
 
 if __name__ == "__main__":
