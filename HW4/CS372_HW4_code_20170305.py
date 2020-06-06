f6b48b25e7796d8caa9b071d2d3d4b2c7d8e2ab8
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-06-06 23:08:02 +0900

    feat(get_tagged_sentences): load sentences.txt

--- HW4/CS372_HW4_code_20170305.py
+++ HW4/CS372_HW4_code_20170305.py
@@ -1,8 +1,42 @@
 import nltk
+import random
+from pprint import pprint
+
+
+def get_tagged_sentences():
+    """Reads pre-processed tagged sentences.
+
+    Returns:
+        sentences (list): each item follows below format;
+            (
+                sentence_text,
+                (X, action, Y)
+            )
+    """
+    f = open('sentences.txt', 'r')
+    lines = f.readlines()
+
+    # parse sentences
+    sentences = []
+    text = None
+    for line in lines:
+        if line.startswith("(Text)"):
+            text = line.strip()[7:]
+        elif line.startswith("(Tags)"):
+            sentences.append(
+                (text, eval(line.strip()[7:]))
+            )
+        else: continue
+    f.close()
+    return sentences
 
 
 def main():
-    pass
+    # divide sentences into train, and test sets.
+    sentences = get_tagged_sentences()
+    random.shuffle(sentences)
+    train = sentences[:80]
+    test = sentences[80:]
 
 
 if __name__ == "__main__":
